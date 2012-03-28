1023318eb0a0c02064c0eb2fef258051f2c0813f
Willson Haw <willsonh@iqmetrix.com>
2012-03-27 20:13:21 -0700

    First checkin with solutions and projects

---
+++ PhotoCache.Web/Modules/HelloModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace PhotoCache.Web.Modules
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = x => "Hello World";
+        }
+    }
+}
